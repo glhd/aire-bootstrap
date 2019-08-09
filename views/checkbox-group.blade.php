c3ba8ac251983789827061a76eb0ae8f7ea9d1f0
Bogdan Kharchenko <scriptor@gmail.com>
2019-08-08 23:48:16 -0400

    check box groups

--- views/checkbox-group.blade.php
+++ views/checkbox-group.blade.php
@@ -2,32 +2,34 @@
 <?php /** @var \Galahad\Aire\Support\OptionsCollection $options */ ?>
 
 <div {{ $attributes->wrapper }}>
-	
-	@isset($prepend_empty_option)
-		<label {{ $attributes->label }}>
-			<input
-					{{ $attributes->except('id', 'value', 'checked') }}
-					value="{{ $prepend_empty_option->value }}"
-					{{ $attributes->isValue($prepend_empty_option->value) ? 'checked' : '' }}
-			/>
-			<span {{ $attributes->label_wrappwer }}>
-				{{ $prepend_empty_option->label }}
-			</span>
-		</label>
-	@endisset
-	
-	@foreach($options->getOptions() as $option_value => $option_label)
-		
-		<label {{ $attributes->label }}>
-			<input
-				{{ $attributes->except('id', 'value', 'checked') }}
-				value="{{ $option_value }}"
-				{{ $attributes->isValue($option_value) ? 'checked' : '' }}
-			/>
-			<span {{ $attributes->label_wrappwer }}>
-				{{ $option_label }}
-			</span>
-		</label>
-	
-	@endforeach
+
+    @isset($prepend_empty_option)
+        <div {{ $attributes->label_wrapper }}>
+            <input
+                {{ $attributes->except('id', 'value', 'checked') }}
+                value="{{ $prepend_empty_option->value }}"
+                {{ $attributes->isValue($prepend_empty_option->value) ? 'checked' : '' }}
+            />
+            <label {{ $attributes->label }}>
+                {{ $prepend_empty_option->label }}
+            </label>
+        </div>
+    @endisset
+
+    @foreach($options->getOptions() as $option_value => $option_label)
+
+        <div {{ $attributes->label_wrapper }}>
+            <input
+                {{ $attributes->except('id', 'value', 'checked') }}
+                value="{{ $option_value }}"
+                {{ $attributes->isValue($option_value) ? 'checked' : '' }}
+                id="aire-selection-{{ \Illuminate\Support\Str::slug($option_value) }}"
+            />
+            <label {{ $attributes->label }}
+                   for="aire-selection-{{ \Illuminate\Support\Str::slug($option_value) }}">
+                {{ $option_label }}
+            </label>
+        </div>
+
+    @endforeach
 </div>
