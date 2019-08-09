9fa4e1a9b15607378cf69d6ca364330e153574f7
Bogdan Kharchenko <scriptor@gmail.com>
2019-08-09 10:15:16 -0400

    tweak radio and checkbox templates

--- views/checkbox-group.blade.php
+++ views/checkbox-group.blade.php
@@ -9,10 +9,12 @@
                 {{ $attributes->except('id', 'value', 'checked') }}
                 value="{{ $prepend_empty_option->value }}"
                 {{ $attributes->isValue($prepend_empty_option->value) ? 'checked' : '' }}
+                id="aire-checkbox-{{ \Illuminate\Support\Str::slug($attributes->get('name').'-'.$prepend_empty_option->value) }}">
             />
-            <label {{ $attributes->label }}>
+            <lable {{ $attributes->label }}
+                   id="aire-checkbox-{{ \Illuminate\Support\Str::slug($attributes->get('name').'-'.$prepend_empty_option->value) }}">
                 {{ $prepend_empty_option->label }}
-            </label>
+            </lable>
         </div>
     @endisset
 
@@ -23,10 +25,10 @@
                 {{ $attributes->except('id', 'value', 'checked') }}
                 value="{{ $option_value }}"
                 {{ $attributes->isValue($option_value) ? 'checked' : '' }}
-                id="aire-selection-{{ \Illuminate\Support\Str::slug($option_value) }}"
+                id="aire-checkbox-{{ \Illuminate\Support\Str::slug($attributes->get('name').'-'.$option_value) }}"
             />
             <label {{ $attributes->label }}
-                   for="aire-selection-{{ \Illuminate\Support\Str::slug($option_value) }}">
+                   for="aire-checkbox-{{ \Illuminate\Support\Str::slug($attributes->get('name').'-'.$option_value) }}">
                 {{ $option_label }}
             </label>
         </div>
--- views/radio-group.blade.php
+++ views/radio-group.blade.php
@@ -2,15 +2,16 @@
 <?php /** @var \Galahad\Aire\Support\OptionsCollection $options */ ?>
 
 @isset($prepend_empty_option)
+
     <div {{ $attributes->label_wrapper }}>
         <input
             {{ $attributes->except('id', 'value', 'checked') }}
             value="{{ $prepend_empty_option->value }}"
             {{ $attributes->isValue($prepend_empty_option->value) ? 'checked' : '' }}
-            id="aire-selection-{{ \Illuminate\Support\Str::slug($prepend_empty_option->label) }}"
+            id="aire-selection-{{ \Illuminate\Support\Str::slug($attributes->get('name').'-'.$prepend_empty_option->label) }}"
         />
         <label {{ $attributes->label }}
-               for="aire-selection-{{ \Illuminate\Support\Str::slug($prepend_empty_option->label) }}">
+               for="aire-selection-{{ \Illuminate\Support\Str::slug($attributes->get('name').'-'.$prepend_empty_option->label) }}">
             {{ $prepend_empty_option->label }}
         </label>
     </div>
@@ -23,10 +24,10 @@
             {{ $attributes->except('id', 'value', 'checked') }}
             value="{{ $option_value }}"
             {{ $attributes->isValue($option_value) ? 'checked' : '' }}
-            id="aire-selection-{{ \Illuminate\Support\Str::slug($option_value) }}"
+            id="aire-selection-{{ \Illuminate\Support\Str::slug($attributes->get('name').'-'.$option_value) }}"
         />
         <label {{ $attributes->label }}
-               for="aire-selection-{{ \Illuminate\Support\Str::slug($option_value) }}">
+               for="aire-selection-{{ \Illuminate\Support\Str::slug($attributes->get('name').'-'.$option_value) }}">
             {{ $option_label }}
         </label>
     </div>
