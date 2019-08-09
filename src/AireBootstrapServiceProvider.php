99e38bc2fb268db0fe6ab1843e4b55b1bdd4dfa6
Bogdan Kharchenko <scriptor@gmail.com>
2019-08-09 10:04:17 -0400

    use custom checkbox and radios fixes issues with validation being displayed for grouped checkboxes and radios.

--- src/AireBootstrapServiceProvider.php
+++ src/AireBootstrapServiceProvider.php
@@ -3,7 +3,6 @@
 namespace Galahad\AireBootstrap;
 
 use Galahad\Aire\Elements\Checkbox;
-use Galahad\Aire\Elements\CheckboxGroup;
 use Galahad\Aire\Support\Facades\Aire;
 use Illuminate\Support\ServiceProvider;
 
@@ -11,22 +10,25 @@ class AireBootstrapServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadViewsFrom(dirname(__DIR__).'/views', 'aire-bootstrap');
-        
+        $this->loadViewsFrom(dirname(__DIR__) . '/views', 'aire-bootstrap');
+
         Aire::setTheme('aire-bootstrap', null, [
             'default_classes' => [
                 'group' => 'form-group',
                 'group_prepend' => '',
                 'group_append' => '',
                 'group_help_text' => 'form-text text-muted',
-                'group_errors' => 'invalid-feedback',
+                'group_errors' => 'not-yet-validated',
                 'label' => '',
                 'input' => 'form-control',
-                'checkbox' => 'form-check-input',
-                'checkbox_label' => 'form-check-label',
-                'checkbox_wrapper' => 'form-check',
-                'checkbox_group_label' => '',
-                'checkbox_group_label_wrapper' => '',
+                'checkbox' => 'custom-control-input',
+                'checkbox_label' => 'custom-control-label',
+                'checkbox_wrapper' => 'custom-control custom-checkbox',
+
+                'checkbox_group' => 'custom-control-input',
+                'checkbox_group_label' => 'custom-control-label',
+                'checkbox_group_label_wrapper' => 'custom-control custom-checkbox',
+
                 'radio_group' => 'custom-control-input',
                 'radio_group_label' => 'custom-control-label',
                 'radio_group_label_wrapper' => 'custom-control custom-radio',
@@ -41,40 +43,39 @@ class AireBootstrapServiceProvider extends ServiceProvider
                     'radio' => 'no-yet-validated',
                     'select' => 'no-yet-validated',
                     'textarea' => 'no-yet-validated',
-                    'group_errors' => 'hidden',
+                    'errors' => 'd-none',
+                    'group_errors' => 'd-none',
                     'group_help_text' => 'no-yet-validated',
                 ],
                 'valid' => [
                     'label' => 'is-valid',
                     'checkbox' => 'is-valid',
                     'radio' => 'is-valid',
+                    'checkbox-group' => 'is-valid',
                     'radio-group' => 'is-valid',
                     'input' => 'is-valid',
                     'select' => 'is-valid',
                     'textarea' => 'is-valid',
-                    'group_errors' => 'valid-feedback',
+                    'errors' => 'valid-feedback',
                     'group_help_text' => 'valid-feedback',
                 ],
                 'invalid' => [
                     'label' => 'is-invalid',
                     'checkbox' => 'is-invalid',
+                    'checkbox-group' => 'is-invalid',
                     'radio-group' => 'is-invalid',
                     'radio' => 'is-invalid',
                     'input' => 'is-invalid',
                     'select' => 'is-invalid',
                     'textarea' => 'is-invalid',
-                    'group_errors' => 'invalid-feedback',
-                    'group_help_text' => 'invalid-feedback',
+                    'errors' => 'invalid-feedback d-block',
+                    'group_help_text' => 'invalid-feedback d-block',
                 ],
             ],
         ]);
-        
-        Checkbox::registerElementMutator(function(Checkbox $checkbox) {
-            $checkbox->group->addClass('form-check');
-        });
-        
-        CheckboxGroup::registerElementMutator(function(CheckboxGroup $checkbox_group) {
-            $checkbox_group->group->addClass('form-check');
+
+        Checkbox::registerElementMutator(function (Checkbox $checkbox) {
+            $checkbox->group->addClass('custom-control custom-checkbox');
         });
     }
 }
