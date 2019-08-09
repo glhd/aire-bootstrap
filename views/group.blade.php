b6b7ddf1f4fd68177bb2c2bc22d02f1fb840bd66
Bogdan Kharchenko <scriptor@gmail.com>
2019-08-09 11:42:18 -0400

    support prepend and append.

--- views/group.blade.php
+++ views/group.blade.php
@@ -3,7 +3,29 @@
 <div {{ $attributes }}>
     {{ $label }}
 
-    {{ $element }}
+    @if($prepend || $append)
+        <div class="input-group">
+            @if($prepend)
+                <div {{ $attributes->prepend }}>
+                    <div class="input-group-text">
+                        {{ $prepend }}
+                    </div>
+                </div>
+            @endif
+
+            {{ $element }}
+
+            @if($append)
+                <div {{ $attributes->append }}>
+                    <div class="input-group-text">
+                        {{ $append }}
+                    </div>
+                </div>
+            @endif
+        </div>
+    @else
+        {{ $element }}
+    @endif
 
     @isset($help_text)
         <small {{ $attributes->help_text }}>
